8cd6fc6be7a6e73ec2d0f02c7cdf4d41f39781d1
Tajra Sinanagic <tajra.sinanagic24@students.kbcc.cuny.edu>
2021-01-14 23:18:35 +0000

    goodnight

---
+++ src/wintern-bios/ExampleComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import RomasComponent from './RomasComponent.jsx';
+import LarasComponent from './LarasComponent.jsx';
+import AndreasComponent from './AndreasComponent.jsx';
+import TajrasComponent from './TajrasComponent.jsx';
+import DanasComponent from './DanasComponent.jsx';
+
+const ExampleComponent = ({bioToShow, color='red'}) => {
+  const { useState } = React;
+
+  const [showStateEx, setShowStateEx] = useState(false);
+
+  const randomNum = Math.random();
+
+
+  return (
+    <div className='changeClassName'>
+
+        { bioToShow === 'Dana' && <DanasComponent /> }
+        { bioToShow === 'Tajra' && <TajrasComponent /> } 
+        { bioToShow === 'Lara' && <LarasComponent /> } 
+        { bioToShow === 'Andrea' && <AndreasComponent /> }
+        { bioToShow === 'Roma' && <RomasComponent /> }
+    </ div>
+  );
+}
+
+
+export default ExampleComponent;
